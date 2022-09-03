0a123ccb9e6fada1b3295ab6bc21ad58ac7d4dd1
Сергей Фомин <xaoc615@gmail.com>
2022-09-03 18:39:16 +0300

    Dop_2-1

---
+++ src/UniversalComponents/Button.tsx
@@ -0,0 +1,14 @@
+
+type ButtonType={
+    name:string
+    callBack:()=>void
+}
+export const Button=(props:ButtonType)=>{
+    const {name,callBack}=props
+    const onClickHandler=()=>{
+        callBack()
+    }
+    return(
+        <button onClick={onClickHandler}>{name}</button>
+    )
+}
